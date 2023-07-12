f3212574b3cd913c273b928a7ff622b805611b7c
sandhya429 <136189486+sandhya429@users.noreply.github.com>
2023-07-12 19:51:10 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,68 +1,75 @@
-function SavedButton(){
-    let wet = document.querySelector(".inputBox1").innerHTML;
-    let set = document.querySelector(".inputBox2").innerHTML;
-    localStorage.setItem(wet,set);
-    console.log(localStorage.getItem(set));
-    set++;
+let savedSolutions = JSON.parse(localStorage.getItem("results")) || [];
+
+let flag = false;
+
+const historyContainer = document.querySelector(".historycon");
+function HistoryButton(e) {
+  flag = !flag;
+  if (flag) {
+    historyContainer.style.display = "block";
+    let data = historyContainer;
+
+    data.textContent = "";
+    savedSolutions.forEach((solution) => {
+      let solutionContainer = document.createElement("div");
+      solutionContainer.className = "container-input";
+
+      solutionContainer.innerHTML = `
+        
+            <div class="inputBox1">${solution.operation} : ${solution.expression}</div>
+            <div class="inputBox2">Solution: ${solution.result}</div>
+            <!-- Create a delete button -->
+            <button id="deleteBtn" >
+                <i class="fa-solid fa-trash-can fa-3x"></i>
+            </button>  
+        
+      `;
+
+      const delBtn = solutionContainer.querySelector("#deleteBtn");
+
+      delBtn.addEventListener("click", (e) => {
+        deleteSol(e, solution);
+      });
+
+      function deleteSol() {
+        delBtn.parentElement.remove();
+        savedSolutions = savedSolutions.filter((sol) => {
+          return sol.expression === solution.expression;
+        });
+        localStorage.setItem("results", JSON.stringify(savedSolutions));
+      }
+      data.appendChild(solutionContainer);
+    });
+  } else {
+    historyContainer.style.display = "none";
+  }
 }
 
-let hcount =0;
-
-function HistoryButton(){
-    hcount++;
-    if(hcount%2 == 1){
-        document.querySelector(".historycon").style.display = "block";
-        let data = document.querySelector(".historycon");
-
-        for(p in localStorage){
-            let ans = localStorage.getItem(p);
-            if(ans != null){
-                // console.log(ans);
-                let h3 = document.createElement("h3");
-                h3.setAttribute('class','remove');
-                h3.innerHTML=`${p} => Result = ${ans}`;
-                console.log(h3);
-                data.appendChild(h3);
-            }
-        }
-    }
-    else{
-        document.querySelector(".historycon").style.display = "none";
-        let rem = document.querySelector(".remove");
-        for(let i=0;i<rem.clientHeight;i++){
-            rem[i].remove();
-        }
-    }
-}
-
-function SearchButton(){
-    event.preventDefault();
+async function SearchButton() {
+  try {
     let problem = document.getElementById("problemBar").value;
     let category = document.getElementById("categoryBar").value;
 
-    let pro = fetch(`https://newton.vercel.app/api/v2/${category}/${problem}`);
-    console.log(pro);
+    const response = await fetch(
+      `https://newton.vercel.app/api/v2/${category}/${problem}`
+    );
 
-    pro.then((response) => {
-        console.log(response.status);
-        console.log(response.ok);
-        return response.json();
-    })
-    .then((value)=>{
-        console.log("Value:",value);
-        let input2 = value.result;
-        document.querySelector(".inputBox2").innerHTML = input2;
-    })
+    const pro = await response.json();
 
-    const input1 = document.querySelector(".inputBox1").innerHTML = category+" : "+problem;
-    let set = document.querySelector(".inputBox1").value;
-    let wet =  document.querySelector(".inputBox2").innerHTML;
+    document.querySelector(".inputBox2").innerHTML = "Solution: " + pro.result;
+    const input1 = (document.querySelector(".inputBox1").innerHTML =
+      category + " : " + problem);
 
-    localStorage.setItem(set,wet);
-}   
-
-function deleteBtn(){
-    console.log("deleteBtn() working")
-    document.querySelector(".inputBox1").innerHTML = "";
-    document.querySelector(".inputBox2").innerHTML="";
+    savedSolutions.push(pro);
+    localStorage.setItem("results", JSON.stringify(savedSolutions));
+  } catch (e) {
+    console.log(e);
+  }
 }
+
+function deleteBtn() {
+  document.querySelector(".inputBox1").innerHTML = "";
+  document.querySelector(".inputBox2").innerHTML = "";
+}
+
+function render() {}
